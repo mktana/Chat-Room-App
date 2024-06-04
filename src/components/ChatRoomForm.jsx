0d954c034589350983dc1c336ed01064af0e0860
moriya <moriyakatan@gmail.com>
2024-06-04 10:27:19 +0300

    changes

--- src/components/ChatRoomForm.jsx
+++ src/components/ChatRoomForm.jsx
@@ -14,77 +14,49 @@ function ChatRoomForm() {
   const [text, setText] = useState('');
   const [message, setMessage] = useState([]);
 
-  // const newMessage = {
-  //   text,
-  //   timestamp: new Date().toISOString(),
-  // };
-
   // HANDLE TEXTFIELD
   const handleChange = (e) => {
-    // if (text === '') {
-    //   setMessage(null);
-    // }
     setText(e.target.value);
-    // console.log(text);
   };
 
   // HANDLE BUTTON
   const handleClick = (e) => {
     e.preventDefault();
-    if(text) {
+    if (text) {
       const newMessage = {
         text,
         timestamp: new Date().toISOString(),
       };
       console.log(newMessage);
-      setMessage((prevMessage) => [...prevMessage, newMessage])
+      setMessage((prevMessage) => [...prevMessage, newMessage]);
     }
     setText('');
-  }
-
-  
-  //Log text to console
-  // const handleClick = (e) => {
-  //   e.preventDefault();
-  //   if (text) {
-  //     const newMessage = {
-  //       text,
-  //     };
-  //     console.log(newMessage);
-  //   }
-  //   setText('');
-  // };
-
-  // Adding new message
-  // const handleAddMessage = () => {
-  //   if (message === '') {
-  //     const newMessage = {
-  //       text,
-  //       time: new Date().toISOString(),
-  //     };
-  //     console.log(message);
-  //     console.log(newMessage);
-  //     setMessage(...message, newMessage);
-  //   }
-  //   console.log();
-  // };
+  };
 
   return (
-    <Container maxWidth='mx-auto'>
-      <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
-        <List style={{ maxHeight: '300px', overflow: 'auto' }}>
-          <ListItem>
-            <ListItemText />
-          </ListItem>
-        </List>
-        <Box display={'flex'} alignItems={'center'} mt={2}>
+    <Container>
+      <Paper elevation={3} style={{ padding: '20px', marginTop: '20px', height: '90vh', display: 'flex', flexDirection: 'column' }}>
+        <Box flexGrow={1} overflow='auto' mb={2} >
+          <List>
+            {message.map((msg, index) => (
+              <ListItem key={index}>
+                <ListItemText
+                  primary={msg.text}
+                  secondary={`${new Date(msg.timestamp).toLocaleTimeString()}`}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box display='flex' alignItems='center'>
           <TextField
+            variant="outlined"
             fullWidth
             label='Add comment'
-            variant='standard'
             type={text}
             onChange={handleChange}
             value={text}
+            style={{}}
           />
           <Button
             variant='contained'
@@ -95,7 +67,7 @@ function ChatRoomForm() {
             Send
           </Button>
         </Box>
-      </Paper>
+        </Paper>
     </Container>
   );
 }
